b03c03641cc8c7e25928af4071bba218f2107545
borinigabriel8 <borinigabriel8@hotmail.com>
2025-04-03 20:36:35 -0300

    Adicionando build/web para deploy no GitHub Pages

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,15 +25,15 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "ff7ad74b8208cbb9951407dcd79673c1",
+"flutter_bootstrap.js": "9b5e5d15cfe05853bfcf4ce1d352b017",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "baeae21632c8499c2f66f6e4293fa555",
-"/": "baeae21632c8499c2f66f6e4293fa555",
+"index.html": "ed92d9d60d8f5c65765aa2342760313c",
+"/": "ed92d9d60d8f5c65765aa2342760313c",
 "main.dart.js": "73afd37e59f1278dd4b4b932132015c6",
-"manifest.json": "8a396b09e3a616115ba8b36b84a7095a",
+"manifest.json": "4421fa12f638e13963ad2bde8430204f",
 "version.json": "81a07f4cebb927ed7904a324626ff5d2"};
 // The application shell files that are downloaded before a service worker can
 // start.
